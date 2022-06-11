fd14f14b3c3696334adec91082963a29c5d2e555
esrasumer <esrasumer28@gmail.com>
2022-06-11 22:25:25 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let price = 100;
+let tax = 0.18;
+let priceTax = price * tax;
+let total = price + priceTax;
+console.log("Fiyat:", price, "KDV Oranı :", tax, "KDV Tutarı:", priceTax,
+    "Fiyat:", total);
+
+
+let counter = 320;
+counter += 1;
+counter++;
+counter--;
+counter *= 2;
+console.log(counter);
+
+// tek mi çift mi ****mod(%)
+
+console.log(15 % 2);
+
+// 8 urun alan koliye tum urunler sıgar mı
+
+console.log("kalan koli :", 18 % 8);
+
+// üs alma
+
+console.log(2 ** 4);
+
+// aşağı yuvarlama 
+console.log(Math.floor(1.9));
+
+// yukarı yuvarlmama
+console.log(Math.ceil(1, 1));
+
+
+// yakına yuvarlama
+
+console.log(Math.round(1.4));
